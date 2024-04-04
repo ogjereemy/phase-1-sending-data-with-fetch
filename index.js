09754b285e559259491d673ce19e15f353eb7304
ogola <ogolajeremy601@gmail.com>
2024-04-04 22:52:53 +0300

    adds-send-lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+let details = {
+    name: "Steve",
+    email: "steve@steve.com",
+}
+function submitData() {
+   return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(details)
+
+    })
+    .then(response => response.json())
+    .then(result => {
+        let newId = result.id;
+        let idElement = document.createElement("p");
+        idElement.textContent = `New ID: ${newId}`;
+        document.body.appendChild(idElement);
+        })
+    .catch(error => {
+        let errorMessage = error.message;
+        let errorElement = document.createElement("p");
+        errorElement.textContent = `Error Message: ${errorMessage}`;
+        document.body.appendChild(errorElement);
+    });
+    // .then((details) => details.forEach(() => ()))
+    // // .then(details => submitData(details))
+    // .catch(error => console.log(error))
+}
+submitData(data)
+
+
